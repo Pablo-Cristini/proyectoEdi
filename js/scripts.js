3df2ae09212b80fe3d4638d02070ccf9032d6c13
pablo cristini <pablitoocristini@gmail.com>
2024-11-26 15:11:48 -0300

    ultimo

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,33 @@ document.getElementById('miFormulario').addEventListener('submit', function (e) 
     confirmButtonText: 'Aceptar'
   });
 });
+
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+document.getElementById('miFormulario').addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  Swal.fire({
+    title: 'Formulario enviado',
+    text: '¡Tu formulario se ha enviado con éxito!',
+    icon: 'success',
+    confirmButtonText: 'Aceptar'
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const clockElement = document.getElementById("clock");
+
+  function updateClock() {
+      const now = new Date();
+      clockElement.textContent = `Hora actual: ${now.toLocaleTimeString()}`;
+  }
+
+  setInterval(updateClock, 1000);
+  updateClock();
+});
